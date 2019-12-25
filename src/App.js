b8d18134b4b834cfaad5100a3f6b0d9ab29433ea
AbhinavSood55 <53464390+AbhinavSood55@users.noreply.github.com>
2019-12-25 12:27:46 -0800

    Push 1
    First Push

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { Component, Fragment } from 'react';
+import { BrowserRouter as Router, Route, NavLink, Link, Prompt, Redirect } from 'react-router-dom';
+import './App.css';
+import Home from './subcomponents/home';
+import Refs_Doms from './subcomponents/refs_doms/refs_doms';
+import { Navbar, Nav } from 'react-bootstrap';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {  }
+  }
+  render() { 
+    return (
+      <Fragment>
+        <Navbar  variant="dark"  bg="primary" expand="lg" sticky="top" >
+          <Navbar.Brand href="/" >React Project</Navbar.Brand>
+          <Nav>
+            <Nav.Link href="/refs_doms"> Refs & Doms</Nav.Link>
+          </Nav>
+        </Navbar>
+        <Router>
+          <Route path="/" exact strict component={Home}/>
+          <Route path="/refs_doms" exact strict component={Refs_Doms}/>
+        </Router>
+      </Fragment>
+    );
+  }
+}
+ 
+export default App;
---
+++ src/subcomponents/refs_doms/refs_doms.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Refs_Doms extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ( <div>REFS & Doms</div> );
+    }
+}
+ 
+export default Refs_Doms;
