871e5f3de8fb3642156128f13e1666ae329628ce
AbhinavSood55 <53464390+AbhinavSood55@users.noreply.github.com>
2019-12-25 22:34:33 -0800

    Changes for Refs and DOMs practice

--- src/subcomponents/refs_doms/refs_doms.js
+++ src/subcomponents/refs_doms/refs_doms.js
@@ -1,4 +1,6 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import { Form, Row, Col } from 'react-bootstrap';
+import "./refs_doms.css";
 
 class Refs_Doms extends Component {
     constructor(props) {
@@ -6,7 +8,43 @@ class Refs_Doms extends Component {
         this.state = {  }
     }
     render() { 
-        return ( <div>REFS & Doms</div> );
+        return ( 
+        <Fragment>
+            <div className="grid">
+                <div className="firstColumn">REFS & Doms</div>
+                    <Form>
+                        <Form.Group as={Row} controlId="ctlID1">
+                            <Col sm="2">
+                                <Form.Label>First field</Form.Label>
+                            </Col>
+                            <Col sm="5">
+                                <Form.Control ref="firstField" type="input" placeholder="First Field" />
+                            </Col>
+                        </Form.Group>
+                        
+                        <Form.Group as={Row} controlId="ctlID2">
+                            <Col sm="2">
+                                <Form.Label>Second field</Form.Label>
+                            </Col>
+                            <Col sm="5">
+                                <Form.Control ref="secondField" type="input" placeholder="Second Field" />
+                            </Col>
+                        </Form.Group>
+
+                        <Form.Group as={Row} controlId="ctlID3">
+                            <Col sm="2">
+                                <Form.Label>Third field</Form.Label>
+                            </Col>
+                            <Col sm="5">
+                                <Form.Control ref="thirdField" type="input" placeholder="Third Field" />
+                            </Col>
+                        </Form.Group>
+
+                    </Form>
+                <div className="lastColumn"></div>
+            </div>
+        </Fragment>
+        );
     }
 }
  
